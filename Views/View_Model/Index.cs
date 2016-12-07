162d55262f8b97206ba7d2782944210cf5d06c1a
tshavor <thomasshavor@gmail.com>
2016-12-07 09:19:51 -0600

    data showing up on DOM

--- Views/View_Model/Index.cs
+++ Views/View_Model/Index.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Data_Dashboard.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,6 @@ namespace Data_Dashboard.Views.View_Model
 {
     public class Index
     {
+       public List<TychoLevel2> TychoLevel2 { get; set; }
     }
 }
