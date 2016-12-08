0ade4a9aad08a69b12aa1fc202f7f5832ecb3b65
tshavor <thomasshavor@gmail.com>
2016-12-08 14:23:18 -0600

    minor changes to views

--- Views/View_Model/Index.cs
+++ Views/View_Model/Index.cs
@@ -9,5 +9,6 @@ namespace Data_Dashboard.Views.View_Model
     public class Index
     {
        public List<TychoLevel2> TychoLevel2 { get; set; }
+       public List<TychoLevel1> TychoLevel1 { get; set; }
     }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,21 @@
 ﻿// Write your Javascript code.
+function getTychoLevel1()
+{
+    return new Promise(function (resolve, reject) {
+        $.ajax({
+            url: "/Home/GetTychoLevel1"  //calling the method to my C# method
+        }).done(function (data) {
+            resolve(data)
+        }).error(function (e) {    //"e" is my error message.
+            reject(e)
+        })
+    })
+}
+
+getTychoLevel1()
+.then(function (ReturnedTychoLevel1data) {
+    console.log(ReturnedTychoLevel1data);
+
+})
+
+
