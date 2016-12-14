41d749ac26b5b3ce367383b667fcace034b9be84
tshavor <thomasshavor@gmail.com>
2016-12-14 13:13:01 -0600

    Graph 1 is operational!

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -70,10 +70,10 @@ $(".submitButton1").on("click", function () {
     var state = $(".selectedState1").val();
     filteredTychoLevel2(year, disease, state)
         .then(function (returndata) {
-            console.log(returndata);
-
-
+            createGraph1(returndata)
+ 
         });
+    //createGraph1(year, disease, state)
 });
 
 $(".submitButton2").on("click", function () {
@@ -102,81 +102,84 @@ $(".submitButton3").on("click", function () {
 
 //GRAPH #1//////// Create a function here that runs on click event of SubmitButton1///
 
-  //function createGraph1 (){
+function createGraph1(monkeybutt) {
+    console.log(monkeybutt);
+    var margin = { top: 40, right: 20, bottom: 30, left: 40 },
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    //var formatPercent = d3.format(".0%");
 
 
+    var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
 
-      var margin = { top: 40, right: 20, bottom: 30, left: 40 },
-          width = 960 - margin.left - margin.right,
-          height = 500 - margin.top - margin.bottom;
+    var y = d3.scale.linear()
+        .range([height, 0]);
 
-      //var formatPercent = d3.format(".0%");
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
 
-      var x = d3.scale.ordinal()
-          .rangeRoundBands([0, width], .1);
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+    //.tickFormat(formatPercent);
 
-      var y = d3.scale.linear()
-          .range([height, 0]);
+    var tip = d3.tip()
+    .attr('class', 'd3-tip')
+    .offset([-10, 0])
+    .html(function (d) {
+        return "<strong>Number per week:</strong> <span style='color:red'>" + d.numberperweekfrequency + "</span>";
+    });
 
-      var xAxis = d3.svg.axis()
-          .scale(x)
-          .orient("bottom");
+    var svg = d3.select(".graph1").append("svg")  //replaced body with "graph1"//
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-      var yAxis = d3.svg.axis()
-          .scale(y)
-          .orient("left")
-      //.tickFormat(formatPercent);
+    svg.call(tip);
 
-      var tip = d3.tip();
-      .attr('class', 'd3-tip')
-      .offset([-10, 0])
-      .html(function (d) {
-          return "<strong>Frequency:</strong> <span style='color:red'>" + d.frequency + "</span>";
-      });
+    //attempting to create route to my JSON data
+   //@Html.Raw(Url.RouteUrl(HomeController.JsonGetTychoLevel1ChartData, new { month = @Model.Month, year = @Model.Year, source=@Model.Source}))
 
-      var svg = d3.select("graph1").append("svg")  //replaced body with "graph1"//
-          .attr("width", width + margin.left + margin.right)
-          .attr("height", height + margin.top + margin.bottom)
-          .append("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    //d3.tsv(monkeybutt, function(error, data){
+        x.domain(monkeybutt.map(function (d) { return parseInt(d.week); }));  //replaced letter with week//
+        y.domain([0, d3.max(monkeybutt, function (d) { return parseInt(d.numberPerWeek); })]);  //replaced frequency with numberperweek//
 
-      svg.call(tip);
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
 
-      var url= Home/GetTychoLevel2ChartData;  //created new variable
-      d3.json("url", type, function (error, data) {  //
-          x.domain(data.map(function (d) { return d.week; }));  //replaced letter with week//
-          y.domain([0, d3.max(data, function (d) { return d.numberperweek; })]);  //replaced frequency with numberperweek//
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+          .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Number per Week"); //replaced frequency with "Number per Week"//
 
-          svg.append("g")
-              .attr("class", "x axis")
-              .attr("transform", "translate(0," + height + ")")
-              .call(xAxis);
+        svg.selectAll(".bar")
+            .data(monkeybutt)
+          .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function (d) { return x(parseInt(d.week)); })   
+        
+        .attr("width", x.rangeBand())
+            .attr("y", function (d) { return y(parseInt(d.numberPerWeek)); }) //replaced frequency with numberperweek//
+            .attr("height", function (d) { return height - y(parseInt(d.numberPerWeek)); })  //replaced frequency with numberperweek//
+                .on('mouseover', tip.show
+                .on('mouseout', tip.hide)
 
-          svg.append("g")
-              .attr("class", "y axis")
-              .call(yAxis)
-            .append("text")
-              .attr("transform", "rotate(-90)")
-              .attr("y", 6)
-              .attr("dy", ".71em")
-              .style("text-anchor", "end")
-              .text("Number per Week"); //replaced frequency with "Number per Week"//
-
-          svg.selectAll(".bar")
-              .data(data)
-            .enter().append("rect")
-              .attr("class", "bar")
-              .attr("x", function (d) { return x(d.week); })  //replaced letter with week//
-              .attr("width", x.rangeBand())
-              .attr("y", function (d) { return y(d.numberperweek); }) //replaced frequency with numberperweek//
-              .attr("height", function (d) { return height - y(d.numberperweek); })  //replaced frequency with numberperweek//
-                  .on('mouseover', tip.show
-                  .on('mouseout', tip.hide)
-
-              );
-
-          function type(d) {
-              d.numberperweek = +d.numberperweek;  //replaced frequency with numberperweek//
-              return d;
-          } [];
-      })
+            );
+        debugger;
+        function type(d) {
+            d.numberPerWeek = +d.numberPerWeek;  //replaced frequency with numberperweek//
+            return d;
+        };
+    //})
+}
